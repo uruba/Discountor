d2fd01d848b0e4eb4efa4f2128cf02994f24ce55
vasek <uruba@outlook.com>
2014-08-10 21:07:29 +0200

    - created separate activity for SavedDiscountsFragment

---
+++ app/src/main/java/cz/uruba/discountor/ActivitySavedDiscounts.java
@@ -0,0 +1,40 @@
+package cz.uruba.discountor;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by vasek on 10/08/14.
+ */
+public class ActivitySavedDiscounts extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_discounts);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+
+        if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new SavedDiscountsFragment()).commit();
+		}
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/cz/uruba/discountor/adapters/ActionBarSpinnerAdapter.java
+++ app/src/main/java/cz/uruba/discountor/adapters/ActionBarSpinnerAdapter.java
@@ -4,13 +4,11 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.util.List;
 
-import cz.uruba.discountor.MainActivity;
 import cz.uruba.discountor.R;
 
 
