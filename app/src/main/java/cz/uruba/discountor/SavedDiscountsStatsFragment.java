1944b91de520b4336bb09a218d9c88dfa147e8e1
Vaclav <uruba@outlook.com>
2014-11-09 23:34:06 +0100

    - imported AndroidPlot

--- app/src/main/java/cz/uruba/discountor/SavedDiscountsStatsFragment.java
+++ app/src/main/java/cz/uruba/discountor/SavedDiscountsStatsFragment.java
@@ -1,10 +1,17 @@
 package cz.uruba.discountor;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
 
 import java.util.ArrayList;
 
@@ -17,6 +24,7 @@ import cz.uruba.discountor.models.item_definitions.DiscountItem;
 
 public class SavedDiscountsStatsFragment extends Fragment {
     private View rootView;
+    private XYPlot chartView;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -30,10 +38,31 @@ public class SavedDiscountsStatsFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_stats_saved_discounts, container,
                 false);
 
+        chartView = (XYPlot) rootView.findViewById(R.id.chart_container);
+
+        renderChart(getGraphSeriesFromDiscountItems(getAllItems()));
+
         return rootView;
     }
 
-    private ArrayList<DiscountItem> getAllItems(){
-        return ModelDiscountItem.getAll(this.getActivity(), true);
+    private ArrayList<DiscountItem> getAllItems() {
+        return ModelDiscountItem.getAll(this.getActivity(), false);
+    }
+
+    private XYSeries getGraphSeriesFromDiscountItems(ArrayList<DiscountItem> discountItems) {
+        ArrayList<Integer> timeList = new ArrayList<Integer>();
+        ArrayList<Double> savingsList = new ArrayList<Double>();
+
+        for (DiscountItem item : discountItems) {
+            timeList.add(item.getDateCreatedUNIXTimestamp());
+            savingsList.add(item.getSavings());
+        }
+
+        return new SimpleXYSeries(timeList, savingsList, "");
+    }
+
+    private void renderChart(XYSeries graphSeries) {
+        //noinspection ResourceType
+        chartView.addSeries(graphSeries, new LineAndPointFormatter(Color.parseColor(getResources().getString(R.color.theme_red)), Color.parseColor(getResources().getString(R.color.theme_red)), Color.WHITE, new PointLabelFormatter(Color.GRAY)));
     }
 }
